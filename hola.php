686d6415971e406f6f0343b5b83c8790110cc690
joseantonioguerreroaragon <65356928+joseantonioguerreroaragon@users.noreply.github.com>
2020-05-14 18:42:21 +0200

    Create hola.php

---
+++ hola.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba DAW</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body id="pagina-login">
+	<?php
+	  echo "Hola, soy Jose Antonio";
+	  ?>
+</body>
+</html>
